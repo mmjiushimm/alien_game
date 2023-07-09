219a9016a78764eb6f235652baa9b945124ff458
mingogo <im.mingogo@gmail.com>
2023-07-09 23:53:29 +0800

    add settings.py to manage all settings

---
+++ settings.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Settings:
+    def __init__(self):
+        self.width = 500
+        self.height = 300
+        self.game_name = 'alien'
+        self.bg_color = pygame.Color(160, 160, 160)
