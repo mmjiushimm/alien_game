0e25cbf4fbe2e88a4f620eed8a52bf2efb778bf0
mingogo <im.mingogo@gmail.com>
2023-07-23 11:03:04 +0800

    settings.py: add ship_speed

--- settings.py
+++ settings.py
@@ -2,7 +2,13 @@ import pygame
 
 class Settings:
     def __init__(self):
-        self.width = 500
-        self.height = 300
+        #显示窗口宽度
+        self.width = 1500
+        #显示窗口高度
+        self.height = 900
+        #显示窗口标题
         self.game_name = 'alien'
+        #显示窗口背景色
         self.bg_color = pygame.Color(160, 160, 160)
+        #ship速度
+        self.ship_speed = 1
